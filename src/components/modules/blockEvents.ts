1e8d81942062800bfceced74e85ba8248e9d9602
Chad Naylor <chad.naylor@gmail.com>
2021-11-15 13:36:31 -0500

    add logging

--- src/components/modules/blockEvents.ts
+++ src/components/modules/blockEvents.ts
@@ -211,7 +211,9 @@ export default class BlockEvents extends Module {
    * @param {KeyboardEvent} event - keydown
    */
   private enter(event: KeyboardEvent): void {
+    console.log('events enter')
     if (document.querySelector('.autocomplete-items>div') != null) {
+      console.log('auto open')
       return;
     }
     const { BlockManager, UI } = this.Editor;
